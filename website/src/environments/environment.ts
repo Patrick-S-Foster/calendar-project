44589f19af93896c629ef2cf3c191aa985af569f
Patrick Foster <patrick.foster@iu-study.org>
2024-12-02 22:04:04 +0100

    Addition of important urls in environment files.

--- website/src/environments/environment.ts
+++ website/src/environments/environment.ts
@@ -1,3 +1,10 @@
 export const environment = {
-    apiUrl: 'http://localhost:8080',
+    logoutUrl: 'http://localhost:8080/logout',
+    registerUrl: 'http://localhost:8080/register',
+    loginUrl: 'http://localhost:8080/login',
+    refreshUrl: 'http://localhost:8080/refresh',
+    createUrl: 'http://localhost:8080/create',
+    getEventsUrl: 'http://localhost:8080/events',
+    updateUrl: 'http://localhost:8080/update',
+    deleteUrl: 'http://localhost:8080/delete',
 };
