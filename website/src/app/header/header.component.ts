5af3e5111ea6395c2e141bb848b8a264c70aeb33
Patrick Foster <patrick.foster@iu-study.org>
2024-12-07 11:08:59 +0100

    Wired up header logout button.

--- website/src/app/header/header.component.ts
+++ website/src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatButton} from "@angular/material/button";
 import {EventService} from "../event.service";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-header',
@@ -14,10 +15,11 @@ import {EventService} from "../event.service";
 })
 export class HeaderComponent {
 
-    constructor(protected eventService: EventService) {
+    constructor(private router: Router, protected eventService: EventService) {
     }
 
     async logout() {
-        return this.eventService.logout();
+        await this.eventService.logout();
+        await this.router.navigate(['login']);
     }
 }
