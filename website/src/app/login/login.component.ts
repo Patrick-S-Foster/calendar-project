55246ed5143e1f4085c1006579ffff84113c5f99
Patrick Foster <patrick.foster@iu-study.org>
2024-12-07 10:51:48 +0100

    Initial implementation of login capabilities.

--- website/src/app/login/login.component.ts
+++ website/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {CredentialFormComponent} from "../credential-form/credential-form.component";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {EventService} from "../event.service";
 
 @Component({
     selector: 'app-login',
@@ -13,4 +14,17 @@ import {RouterLink} from "@angular/router";
 })
 export class LoginComponent {
 
+    @ViewChild('loginForm') loginForm!: CredentialFormComponent;
+
+    constructor(private router: Router, private eventService: EventService) {
+    }
+
+    async login(data: { email: string; password: string }) {
+        if (await this.eventService.login(data.email, data.password)) {
+            // TODO: navigate with the router to the calendar page.
+            return;
+        }
+
+        this.loginForm.errors = ['Could not log in. Please check your email & password, then try again.'];
+    }
 }
