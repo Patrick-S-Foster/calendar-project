6f1b887c4ade757391e9b7b27ee91b220749b9ef
Patrick Foster <patrick.foster@iu-study.org>
2024-12-07 11:38:34 +0100

    Initial implementation of reverse authentication guard.

--- website/src/app/app.routes.ts
+++ website/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {CalendarComponent} from "./calendar/calendar.component";
 import {AuthenticationGuard} from "./authentication.guard";
+import {ReverseAuthenticationGuard} from "./reverse-authentication.guard";
 
 export const routes: Routes = [
-    {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent, canActivate: [ReverseAuthenticationGuard]},
+    {path: 'register', component: RegisterComponent, canActivate: [ReverseAuthenticationGuard]},
     {path: '', component: CalendarComponent, canActivate: [AuthenticationGuard]},
 ];
