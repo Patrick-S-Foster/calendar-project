bec76c76112d75a1cf720f59d72185df77d27c45
Patrick Foster <patrick.foster@iu-study.org>
2024-12-08 18:41:52 +0100

    Initial implementation of the calendar, with a location for calendar cells to be located.

--- website/src/app/calendar/calendar.component.ts
+++ website/src/app/calendar/calendar.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {EventService} from "../event.service";
 
 @Component({
-  selector: 'app-calendar',
-  imports: [],
-  templateUrl: './calendar.component.html',
-  styleUrl: './calendar.component.scss'
+    selector: 'app-calendar',
+    imports: [
+        MatIcon,
+        MatIconButton
+    ],
+    templateUrl: './calendar.component.html',
+    styleUrl: './calendar.component.scss'
 })
 export class CalendarComponent {
 
+    protected readonly months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    constructor(protected eventService: EventService) {
+    }
+
+    async previousMonth() {
+        let month = this.eventService.currentMonth - 1;
+        let year = this.eventService.currentYear;
+
+        if (month <= 0) {
+            month = 12;
+            year--;
+        }
+
+        await this.eventService.setEventRange(month, year);
+    }
+
+    async nextMonth() {
+        let month = this.eventService.currentMonth + 1;
+        let year = this.eventService.currentYear;
+
+        if (month > 12) {
+            month = 1;
+            year++;
+        }
+
+        await this.eventService.setEventRange(month, year);
+    }
 }
