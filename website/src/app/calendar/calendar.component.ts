dab34f2c6ba017a70c0e47f70fff9c019e0ea747
Patrick Foster <patrick.foster@iu-study.org>
2024-12-10 17:32:31 +0100

    Initial implementation of cells, and initial wiring of cells in the calendar.

--- website/src/app/calendar/calendar.component.ts
+++ website/src/app/calendar/calendar.component.ts
@@ -2,12 +2,18 @@ import {Component} from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
 import {MatIconButton} from "@angular/material/button";
 import {EventService} from "../event.service";
+import {NgForOf} from "@angular/common";
+import {TimesPipe} from "../times.pipe";
+import {CellComponent} from "../cell/cell.component";
 
 @Component({
     selector: 'app-calendar',
     imports: [
         MatIcon,
-        MatIconButton
+        MatIconButton,
+        NgForOf,
+        TimesPipe,
+        CellComponent
     ],
     templateUrl: './calendar.component.html',
     styleUrl: './calendar.component.scss'
@@ -16,7 +22,17 @@ export class CalendarComponent {
 
     protected readonly months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+    protected startOffset = 0;
+    protected dayCount = 0;
+
     constructor(protected eventService: EventService) {
+        this.setProperties();
+    }
+
+    private setProperties() {
+        this.startOffset = new Date(this.eventService.currentYear, this.eventService.currentMonth - 1).getDay() + 1;
+        this.dayCount = new Date(this.eventService.currentYear, this.eventService.currentMonth, 0).getDate();
+        console.log(this.dayCount);
     }
 
     async previousMonth() {
@@ -29,6 +45,7 @@ export class CalendarComponent {
         }
 
         await this.eventService.setEventRange(month, year);
+        this.setProperties();
     }
 
     async nextMonth() {
@@ -41,5 +58,6 @@ export class CalendarComponent {
         }
 
         await this.eventService.setEventRange(month, year);
+        this.setProperties();
     }
 }
---
+++ website/src/app/cell/cell.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+import {CalendarEvent} from "../calendarEvent";
+
+@Component({
+    selector: 'app-cell',
+    imports: [],
+    templateUrl: './cell.component.html',
+    styleUrl: './cell.component.scss'
+})
+export class CellComponent {
+
+    @Input({required: true}) number = 0;
+    @Input({required: true}) events: CalendarEvent[] = [];
+
+}
